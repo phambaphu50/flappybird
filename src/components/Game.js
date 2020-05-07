35ec1b7ab01549a7233ae67878efaa71ed3940b4
Pham Ba Phu <phambaphu50@gmail.com>
2020-05-07 22:27:28 +0700

    update status gameover

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,7 +6,14 @@ import Pipe from './Pipe'
 import Foreground from './Foreground'
 import BgImage from '../asset/images/bg.png'
 
+let gameLoop
+let pipeGenerate
+
 const Game = ({status, start, fly}) => {
+    if(status === 'game-over') {
+        clearInterval(gameLoop)
+        clearInterval(pipeGenerate)
+    }
     const handlePress = (e) => {
         if(e.keyCode === 32) {
             fly()
@@ -42,16 +49,50 @@ const fly = (e) => {
     }
 }
 
+const check = (dispatch, getState) => {
+    const state = getState()
+    const birdY = state.bird.y
+    const pipes = state.pipe.pipes
+    const x = state.pipe.x
+
+    const challenge = pipes.map(({topHeight}, i) => {
+        return {
+            x1: x + i * 200,
+            y1: topHeight,
+            x2: x + i * 200,
+            y2: topHeight + 100
+        }
+    }).filter(({x1}) => {
+        if(x1 > 0 && x1 < 288) {
+            return true;
+        }
+    })
+
+    if(birdY > 512 - 100) {
+        dispatch({type: 'GAME-OVER'})
+    }
+
+    if(challenge.length) {
+        const {x1, y1, x2, y2} = challenge[0];
+        if(x1 < 120 && 120 < x1 + 52 && birdY < y1 || 
+          x2 < 120 && 120 < x2 + 52 && birdY > y2) {
+            dispatch({type: 'GAME-OVER'}) 
+          }
+    }
+}
+
 const start = (e) => {
     return (dispatch, getState) => {
         const { status } = getState().game;
         if(status !== 'playing') {
-            setInterval(() => {
+            gameLoop = setInterval(() => {
                 dispatch({type: 'FALL'}) 
                 dispatch({type: 'RUNNING'}) 
+
+                check(dispatch, getState)
             }, 300)
 
-            setInterval(() => {
+            pipeGenerate = setInterval(() => {
                 dispatch({type: 'GENERATE'})
             }, 3000)
 
--- src/reducer/game.js
+++ src/reducer/game.js
@@ -2,6 +2,8 @@ export default (state = {}, {type} = {}) => {
     switch(type) {
         case "START":
             return {...state, status: 'playing'}
+        case "GAME-OVER":
+            return {...state, status: 'game-over'}
         default:
             return state
     }
